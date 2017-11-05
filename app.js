ae7162075f31fc975bc9c2510c2e981da781b1ea
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-04 21:46:52 -0700

    express: start listening

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var favicon = require('serve-favicon');
 const logger = require('heroku-logger');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const jsforce = require('jsforce');
+const conn = new jsforce.Connection();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, function (err, res) {
+  if (err) {
+    return console.log(err);
+  } else {
+    return console.log(res);
+  }
+});
+
 
 var app = express();
 
@@ -14,16 +22,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
+/* GET home page. */
+app.get('/', function (req, res, next) {
+  res.render('index', { title: 'Platform Event Generator' });
+});
+
+// post an empty body the the /events endpoint.  Event is defined in config properties
+app.get('/events', function (req, res, next) {
+  conn.sobject(process.env.EVENT_API_NAME).create(JSON.parse(process.env.EVENT_JSON), function (err, ret) {
+    if (err) {
+      return logger.error(err);
+      res.send(err);
+    } else if (!ret.success) {
+      return logger.error(ret);
+      res.send(ret);
+    } else {
+      res.send(ret);
+    }
+  });
+})
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +67,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+
+const port = process.env.PORT || 8443;
+
+app.listen(port, () => {
+  logger.info(`Example app listening on port ${port}!`);
+});
